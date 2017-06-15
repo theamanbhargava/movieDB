f9a873032b11e62f33bc333c9f821fd822335bdb
Aman Bhargava <aman56493@gmail.com>
2017-06-15 23:47:46 +0530

    Update some fields

--- movieApp/app.js
+++ movieApp/app.js
@@ -5,7 +5,6 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 var path = require('path');
-var request = require("request");
 var validator = require('validator');
 
 app.set("view engine", "ejs");
